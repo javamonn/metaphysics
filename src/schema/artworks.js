9d84291c609bddd5582b6c04f943dc84e2fa09aa
Daniel Ramirez <javamonn@gmail.com>
2018-11-14 12:48:44 -0500

    feat: expose slugs argument on artworks

--- src/schema/artworks.js
+++ src/schema/artworks.js
@@ -1,5 +1,6 @@
 import Artwork from "./artwork"
 import { GraphQLList, GraphQLString } from "graphql"
+import config from "config"
 
 const Artworks = {
   type: new GraphQLList(Artwork.type),
@@ -8,9 +9,35 @@ const Artworks = {
     ids: {
       type: new GraphQLList(GraphQLString),
     },
+    slugs: {
+      type: new GraphQLList(GraphQLString),
+      description: `
+        Only return artworks matching specified slugs.
+        Accepts list of slugs. (e.g. 'andy-warhol', 'banksy')
+      `,
+    },
   },
-  resolve: (root, options, request, { rootValue: { artworksLoader } }) =>
-    artworksLoader(options),
+  resolve: (
+    root,
+    options,
+    request,
+    { rootValue: { artworksLoader, artworkLoader } }
+  ) => {
+    if (options.slugs) {
+      return Promise.all(
+        options.slugs.map(slug =>
+          artworkLoader(
+            slug,
+            {},
+            {
+              requestThrottleMs: config.ARTICLE_REQUEST_THROTTLE_MS,
+            }
+          )
+        )
+      )
+    }
+    return artworksLoader(options)
+  },
 }
 
 export default Artworks
